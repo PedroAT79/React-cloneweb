a585ae697259c78379bb5629e8b214efcea7fa2b
PedroAT79 <plat1505@gmail.com>
2023-10-18 13:45:49 +0200

    Inclusion de props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,27 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
       <h1>Esto es lo que dicen nuestros alumnos</h1>
-      <Testimonio/>
+      <Testimonio
+      nombre= 'Sarah Bostion'
+      pais= 'Suecia'
+      imagen= 'sarah'
+      cargo= 'Ingeniero de Software'
+      empresa= 'Spotify'
+      testimonio='Siempre me ha costado aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'/>
+      <Testimonio
+      nombre= 'Shawn Garcia'
+      pais= 'España'
+      imagen= 'shawn'
+      cargo= 'Analista de datos'
+      empresa= 'Indra'
+      testimonio='Da miedo cambiar de carrera. Solo gané confianza en que podía codificar trabajando durante cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida . '/>
+            <Testimonio
+      nombre= 'Emma Garcia'
+      pais= 'Italia'
+      imagen= 'emma'
+      cargo= 'Analista de datos'
+      empresa= 'Pmodoro'
+      testimonio='Da miedo cambiar de carrera. Solo gané confianza en que podía codificar trabajando durante cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida . '/>
       </div>
     </div>
   );
--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import '../hoja-de-estilos/Testimonio.css'
 
-function Testimonio(){
+function Testimonio(props){
     return (
         <div className= 'contenedor-testimonio'>
             <img className= 'imagen-testimonio'
-             src={require('../imagenes/testimonio-shawn.png')}
+             src={require(`../imagenes/testimonio-${props.imagen}.png`)}
              alt= 'Foto de Shawn'/>
             <div className='contenedor-texto-testimonio'>
                 <p className='nombre-testimonio'>
-                    Shawn Wang in Singapore
+                    {props.nombre} en {props.pais}
                 </p>
                 <p className="cargo-testimonio">
-                    Software Engineer at Amazon
+                    {props.cargo} en {props.empresa}
                 </p>
                 <p className="texto-testimonio">
-                "It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+                "{props.testimonio}"
                 </p>
             </div>
         </div>

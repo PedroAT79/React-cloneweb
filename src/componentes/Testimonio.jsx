204d0f7fe3cb58c1e92983793fc7112c7826723a
PedroAT79 <plat1505@gmail.com>
2023-10-18 12:53:42 +0200

    Primeros pasos

---
+++ src/componentes/Testimonio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Testimonio(){
+    return (
+        <div className= 'contenedor-testimonio'>
+            <img className= 'imagen-testimonio'
+             src={requestAnimationFrame('../imagenes/testimonio-shawn.png')}
+             alt= 'Foto de Shawn'/>
+        </div>
+    );
+}
